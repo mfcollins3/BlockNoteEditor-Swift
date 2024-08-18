c2104b50d6e1e5033ab7b1b3a036fe78db6820b3
Michael Collins <mfcollins3@me.com>
2024-08-18 16:24:28 -0700

    feat: host BlockNote editor in the web view
    I followed the BlockNote quick start to create a basic BlockNote editor
    hosted in a web view. I can use the basic set of blocks.

--- Sources/editor/vite.config.ts
+++ Sources/editor/vite.config.ts
@@ -24,6 +24,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    emptyOutDir: true,
     outDir: '../BlockNote/Resources/Editor'
   },
   plugins: [react()],
